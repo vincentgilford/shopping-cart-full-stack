8f03fe59bba1f3fb0b017e6747050f7e529c8627
JDSalisbury <salisbury.jeffery@gmail.com>
2018-03-22 13:41:01 -0400

    Shell Spring test ran

--- src/test/java/org/wecancodeit/shoppingcartfullstack/StoreAppWiringTest.java
+++ src/test/java/org/wecancodeit/shoppingcartfullstack/StoreAppWiringTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShoppingCartFullStackApplicationTests {
+public class StoreAppWiringTest {
 
 	@Test
 	public void contextLoads() {
