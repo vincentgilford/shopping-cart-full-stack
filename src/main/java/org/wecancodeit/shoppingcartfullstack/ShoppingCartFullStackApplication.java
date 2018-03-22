bbf8d9f02299400a60d843b3435bbb6e93acd991
JDSalisbury <salisbury.jeffery@gmail.com>
2018-03-22 13:32:14 -0400

    Fist push

---
+++ src/main/java/org/wecancodeit/shoppingcartfullstack/ShoppingCartFullStackApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.shoppingcartfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartFullStackApplication.class, args);
+	}
+}
