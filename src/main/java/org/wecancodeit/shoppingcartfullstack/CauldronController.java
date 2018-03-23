17741bdf4f6bf7cb65af5a43b51707939a356e4f
JDSalisbury <salisbury.jeffery@gmail.com>
2018-03-23 10:34:00 -0400

    All test passing, green; request mapping potion correctly

--- src/main/java/org/wecancodeit/shoppingcartfullstack/CauldronController.java
+++ src/main/java/org/wecancodeit/shoppingcartfullstack/CauldronController.java
@@ -3,6 +3,10 @@ package org.wecancodeit.shoppingcartfullstack;
 import java.util.Collection;
 import java.util.Collections;
 
+import javax.annotation.Resource;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class CauldronController {
 	
 	
+	
 	@RequestMapping("/potions")
 	public Collection<Potion> getPotions() {
 		
 		return Collections.singleton(new Potion("Stuff"));
 	}
+	
+	@RequestMapping("/potions/{id}")
+	public Potion getIndividualPoitionArbitraryId(@PathVariable(name="id") long id) {
+		return new Potion("test potion");
+		
+		
+	}
+	
 
 }
